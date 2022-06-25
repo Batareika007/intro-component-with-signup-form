03eaac58ff4fb50fe534b169fbe5501f5ca90b52
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-06-25 15:36:57 +0300

    finished responsive style
    w8 for JS form code

--- script/script.js
+++ script/script.js
@@ -1,20 +1,28 @@
 const inputInner = document.querySelectorAll('.form__inp');
+const errorText = document.querySelectorAll('.form__error-text');
+
+
 const fname = document.querySelector('#form-fname');
 const lname = document.querySelector('#form-lname');
 const femail = document.querySelector('#form-email');
 const fpass = document.querySelector('#form-password');
 
+// add error sigh for every line
 
 inputInner.forEach(error => {
   error.classList.remove('error__sign');
-  
+});
+
+errorText.forEach(error => {
+  error.innerHTML = '';
 });
 
 
 
-if (fname === null){
-    document.querySelector('.form__error-text').innerHTML = 'Enter Name'
-    // inputInner.classList.add('error__sign');
+
+if (fname === null) {
+  document.querySelector('.form__error-text').innerHTML = 'Enter Name'
+  // inputInner.classList.add('error__sign');
 }
 
 // if (lname === null){
