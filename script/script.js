b3588c6fbe5f230130b65a95e796b8b8458aea54
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-07-03 00:18:47 +0300

    testing scripts

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
 const inputInner = document.querySelectorAll('.form__inp');
 const errorText = document.querySelectorAll('.form__error-text');
 
-
+// form first name
 const fname = document.querySelector('#form-fname');
+// const fname2 = document.forms['main-form']['First_Name'];
+// form last name
 const lname = document.querySelector('#form-lname');
+// form email
 const femail = document.querySelector('#form-email');
+// form password
 const fpass = document.querySelector('#form-password');
 
 // add error sigh for every line
@@ -16,14 +20,48 @@ inputInner.forEach(error => {
 errorText.forEach(error => {
   error.innerHTML = '';
 });
+// form name
+fname.addEventListener("input", function (event) {
+  if (fname.value === '') {
+    fname.setCustomValidity("First name cannot be empty");
+    fname.reportValidity("");
+  } else {
+    fname.setCustomValidity("");
+  }
+});
+// form last name
+lname.addEventListener("input", function (event) {
+  if (lname.validity.typeMismatch) {
+    lname.setCustomValidity("Last name cannot be empty");
+    lname.reportValidity();
+  } else {
+    lname.setCustomValidity("");
+  }
+});
+// form email
+femail.addEventListener("input", function (event) {
+  if (femail.validity.typeMismatch) {
+    femail.setCustomValidity("Looks like it not an email");
+    femail.reportValidity();
+  } else {
+    femail.setCustomValidity("");
+  }
+});
+// form password
+fpass.addEventListener("input", function (event) {
+  if (fpass.validity.typeMismatch) {
+    fpass.setCustomValidity("Password cannot be empty");
+    fpass.reportValidity();
+  } else {
+    fpass.setCustomValidity("");
+  }
+});
 
 
-
-
-if (fname === null) {
-  document.querySelector('.form__error-text').innerHTML = 'Enter Name'
-  // inputInner.classList.add('error__sign');
-}
+// if (fname === null) {
+//   document.querySelector('.form__error-text').innerHTML = 'Enter Name'
+//   // inputInner.classList.add('error__sign');
+// }
 
 // if (lname === null){
 //     document.querySelector('.form__error-text').innerHTML = 'Enter Last Name'
